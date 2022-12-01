13a7ede290bce93ec529949afbce5fe21055d396
Morisson Marcel <@morissonmaciel>
2022-12-01 09:37:00 -0300

    Basic project structure proposal

---
+++ iOSApp/Sources/Scenes/RootScene.swift
@@ -0,0 +1,25 @@
+//
+//  RootScene.swift
+//  Papered
+//
+//  Created by Morisson Marcel on 30/11/22.
+//
+
+import SwiftUI
+
+struct RootScene: View {
+    var body: some View {
+        Text("Hello, world!")
+            .font(.largeTitle)
+    }
+}
+
+#if DEBUG
+
+struct RootScene_Previews: PreviewProvider {
+    static var previews: some View {
+        RootScene()
+    }
+}
+
+#endif
